fa30eb3d22e9d3875447c52b3eda3116bfc183b5
Min Jun <qkralswns931026@gmail.com>
2023-09-02 16:35:15 +0900

    [DEV] DEMO 씬 작업 중 - 1

---
+++ CurveSettings/Assets/Game/Scripts/CurveView.cs
@@ -0,0 +1,23 @@
+// ----- C#
+using System.Collections;
+using System.Collections.Generic;
+
+// ----- Unity
+using UnityEngine;
+
+namespace InGame.ForUI
+{
+    public class CurveView : MonoBehaviour
+    {
+        // --------------------------------------------------
+        // Components
+        // --------------------------------------------------
+        [SerializeField] RectTransform _RECT_Frame = null;
+
+        // --------------------------------------------------
+        // Functinos - Nomal
+        // --------------------------------------------------
+
+
+    }
+}
---
+++ CurveSettings/Assets/Game/Scripts/Main.cs
@@ -0,0 +1,14 @@
+// ----- C#
+using System.Collections;
+using System.Collections.Generic;
+
+// ----- Unity
+using UnityEngine;
+
+namespace InGame
+{
+    public class Main : MonoBehaviour
+    {
+
+    }
+}
